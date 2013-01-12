411b5b4afa7006663540c216a3978c9c723106bb
Zbigniew Siciarz <antyqjon@gmail.com>
2013-01-12 23:43:30 +0100

    Fixed urls

--- siciarz/urls.py
+++ siciarz/urls.py
@@ -7,9 +7,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include('articles.urls', namespace='articles')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^rosetta/', include('rosetta.urls'))
+    url(r'^rosetta/', include('rosetta.urls')),
+
+    url(r'^', include('articles.urls', namespace='articles')),
 )
