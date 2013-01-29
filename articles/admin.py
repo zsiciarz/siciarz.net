981b137f21d9869c508945ac84262d252ff92f42
Zbigniew Siciarz <antyqjon@gmail.com>
2013-01-29 22:16:29 +0100

    Global revision log.

--- articles/admin.py
+++ articles/admin.py
@@ -8,10 +8,24 @@ Administration for articles.
 """
 
 from django.contrib import admin
+from django.shortcuts import redirect
 
 import reversion
+from reversion.models import Revision
 
 from .models import Article
+
+
+class RevisionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'comment', 'date_created')
+    search_fields = ('=user__username', '=user__email')
+    date_hierarchy = ('date_created')
+
+    def change_view(self, request, obj=None):
+        self.message_user(request, 'You cannot change history.')
+        return redirect('admin:reversion_revision_changelist')
+
+admin.site.register(Revision, RevisionAdmin)
 
 
 class ArticleAdmin(reversion.VersionAdmin):
