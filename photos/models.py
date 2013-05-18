8131a2a894359120b4a983a0297f098c68ac6d51
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-18 21:59:47 +0200

    Added initial code for photo galleries.

---
+++ photos/models.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Zbigniew Siciarz 2009-2013.
+
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+from django.db.models.query import QuerySet
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from markitup.fields import MarkupField
+from model_utils import Choices
+from model_utils.managers import PassThroughManager
+from model_utils.models import StatusModel, TimeStampedModel
+
+
+class GalleryQuerySet(QuerySet):
+    def published(self):
+        return self.filter(status='published')
+
+
+@python_2_unicode_compatible
+class Gallery(StatusModel, TimeStampedModel):
+    STATUS = Choices(
+        ('draft', _("draft")),
+        ('published', _("published")),
+    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("author"))
+    title = models.CharField(_("title"), max_length=255)
+    slug = models.SlugField(_("slug"), max_length=255, unique=True)
+    description = MarkupField(_("description"))
+
+    objects = PassThroughManager.for_queryset_class(GalleryQuerySet)()
+
+    class Meta:
+        verbose_name_plural = _("Galleries")
+        ordering = ['-created']
+
+    def __str__(self):
+        """
+        The string representation of a gallery is its title.
+        """
+        return self.title
+
+
+@python_2_unicode_compatible
+class Photo(TimeStampedModel):
+    gallery = models.ForeignKey(Gallery, null=True, related_name='photos', verbose_name=_("gallery"))
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("author"))
+    title = models.CharField(_("title"), max_length=255)
+
+    class Meta:
+        verbose_name_plural = _("Photos")
+        ordering = ['-created']
+
+    def __str__(self):
+        """
+        The string representation of a photo is its title.
+        """
+        return self.title
