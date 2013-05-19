92d612be4427a0f3b2258474144c0223b6687fdb
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-19 11:37:11 +0200

    Added templatetag to get archive dates for galleries.

--- photos/templatetags/photo_tags.py
+++ photos/templatetags/photo_tags.py
@@ -16,3 +16,11 @@ def get_recent_galleries(count=3):
     Returns most recent galleries.
     """
     return Gallery.objects.published().order_by('-created')[:count]
+
+
+@register.assignment_tag
+def get_gallery_archive_dates():
+    """
+    Returns datetime objects for all months in which galleries were added.
+    """
+    return Gallery.objects.published().dates('created', 'month', order='DESC')
