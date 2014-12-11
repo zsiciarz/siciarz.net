e860d47f33026428917787107d94388e416fcf74
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-11 20:10:30 +0100

    More improvements to the hack.

--- articles/filters.py
+++ articles/filters.py
@@ -5,8 +5,9 @@ import markdown
 
 def markitup_filter(content, *args, **kwargs):
     content = markdown.markdown(content, *args, **kwargs)
-    return re.sub(
+    content = re.sub(
         r'<span class="o">&amp;</span><span class="n">amp</span><span class="p">;(.*)</span>',
         r'<span class="o">&amp;</span><span class="p">\1</span>',
         content
     )
+    return content.replace('<span class="n">amp</span><span class="p">;</span>', '')
