6c21b7e41dacc880a7271ee9f1dc3e7597b51dc8
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-11 19:38:41 +0100

    Improving the hack...

--- articles/filters.py
+++ articles/filters.py
@@ -1,9 +1,12 @@
+import re
+
 import markdown
 
 
 def markitup_filter(content, *args, **kwargs):
     content = markdown.markdown(content, *args, **kwargs)
-    return content.replace(
-        '<span class="o">&amp;</span><span class="n">amp</span><span class="p">;</span>',
-        '<span class="o">&amp;</span>',
+    return re.sub(
+        r'<span class="o">&amp;</span><span class="n">amp</span><span class="p">;(.*)</span>',
+        r'<span class="o">&amp;</span><span class="p">\1</span>',
+        content
     )
