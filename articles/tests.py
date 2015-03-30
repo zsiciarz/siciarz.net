da6c54ad6a468cdb56a054a586d06e48aafad1b9
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-03-30 17:28:32 +0200

    Test for update_pageviews() method.

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from .models import Article
@@ -7,3 +8,11 @@ class ArticleTestCase(TestCase):
     def test_str(self):
         article = Article(title="Hello world!")
         self.assertEqual(str(article), article.title)
+
+    def test_update_pageviews(self):
+        user = User.objects.create_user(username='user', password='password')
+        article = Article.objects.create(title="Hello", author=user)
+        self.assertEqual(article.pageviews, 0)
+        article.update_pageviews()
+        article = Article.objects.get(pk=article.pk)
+        self.assertEqual(article.pageviews, 1)
