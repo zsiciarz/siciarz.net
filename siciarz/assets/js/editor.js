402c4d89a5882f30ce5e2184449a7f99468fe48f
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-02-20 22:10:39 +0100

    Modernize editor.js

--- siciarz/assets/js/editor.js
+++ siciarz/assets/js/editor.js
@@ -1,18 +1,17 @@
 // import EpicEditor doesn't work - the source doesn't explicitly export it :(
 import NotReallyAnEpicEditor from 'epiceditor/epiceditor/js/epiceditor.min.js'
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    var ids = ['id_summary', 'id_content'];
-    var basePath = document.getElementById('article-form').dataset.editorBasePath;
-    ids.forEach(function(textareaId) {
-        var textarea = document.getElementById(textareaId),
-            containerId = 'epic_' + textareaId,
-            container = document.createElement('div'),
-            label = document.querySelector('label[for="' + textareaId + '"]');
-        textarea.style.display = 'none';
-        container.id = containerId;
-        label.parentNode.appendChild(container);
-        var opts = {
+document.addEventListener('DOMContentLoaded', e => {
+    const ids = ['id_summary', 'id_content']
+    const basePath = document.getElementById('article-form').dataset.editorBasePath
+    ids.forEach(textareaId => {
+        document.getElementById(textareaId).style.display = 'none'
+        const containerId = `epic_${textareaId}`
+        const label = document.querySelector(`label[for="${textareaId}"]`)
+        let container = document.createElement('div')
+        container.id = containerId
+        label.parentNode.appendChild(container)
+        let opts = {
             container: containerId,
             textarea: textareaId,
             basePath: basePath,
@@ -20,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             button: {
                 preview: false
             }
-        };
-        var editor = new EpicEditor(opts).load();
-    });
-});
+        }
+        new EpicEditor(opts).load()
+    })
+})
