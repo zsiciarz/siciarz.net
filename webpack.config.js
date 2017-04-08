ab6ffe1557a05a7801f11f7683b30aa2d017c51e
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-04-08 22:03:07 +0200

    Upgrade to Webpack 2.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -9,30 +10,42 @@ module.exports = {
         style: './siciarz/assets/css/main.less'
     },
     output: {
-        path: './siciarz/assets/build',
+        path: path.resolve(__dirname, './siciarz/assets/build'),
         filename: '[name].js'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
-            query: {
+            options: {
                 presets: ['es2015', 'stage-0']
             }
         }, {
             test: /\.less$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader!less-loader'
+            })
         },
         {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'url-loader?limit=10000&minetype=application/font-woff'
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader',
+            options: {
+                'limit': 100000
+            }
         }, {
-            test: /\.(png|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'file-loader'
-        }, {
-            test: require.resolve("jquery"),
-            loader: "expose?$!expose?jQuery"
+            test: require.resolve('jquery'),
+            use: [
+                {
+                    loader: 'expose-loader',
+                    query: 'jQuery'
+                },
+                {
+                    loader: 'expose-loader',
+                    query: '$'
+                }
+            ]
         }]
     },
     plugins: [
