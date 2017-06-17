45104c6192aa52b552c5f31604b5e4febb971934
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-06-17 20:45:00 +0200

    Replace assignment_tag decorator with simple_tag.

--- articles/templatetags/article_tags.py
+++ articles/templatetags/article_tags.py
@@ -7,7 +7,7 @@ from articles.models import Article
 register = template.Library()
 
 
-@register.assignment_tag
+@register.simple_tag
 def get_top_articles(count=3):
     """
     Returns top articles by pageview count.
@@ -15,7 +15,7 @@ def get_top_articles(count=3):
     return Article.objects.only_articles().published().order_by('-pageviews')[:count]
 
 
-@register.assignment_tag
+@register.simple_tag
 def get_related_articles(article, count=3):
     """
     Returns articles with similar set of tags as the given article.
@@ -26,7 +26,7 @@ def get_related_articles(article, count=3):
     return [obj for obj in queryset if obj.common_tag_count > 0][:count]
 
 
-@register.assignment_tag
+@register.simple_tag
 def get_archive_dates():
     """
     Returns datetime objects for all months in which articles were written.
