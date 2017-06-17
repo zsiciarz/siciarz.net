301ca681769996edcfbf432e11780f072c1f3895
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-06-17 20:45:25 +0200

    Remove admindocs integration.

--- siciarz/urls.py
+++ siciarz/urls.py
@@ -13,7 +13,6 @@ from pgallery.sitemaps import GallerySitemap, PhotoSitemap
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^markitup/', include('markitup.urls')),
