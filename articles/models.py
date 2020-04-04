986e0d7166046bfa9b74a61d0f5b43c6ec424395
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-04-04 17:58:10 +0200

    Clean up URLconf-related deprecation warnings.

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import F
 from django.db.models.query import QuerySet
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from markitup.fields import MarkupField
@@ -64,9 +65,8 @@ class Article(StatusModel, TimeStampedModel):
     def __str__(self):
         return self.title
 
-    @models.permalink
     def get_absolute_url(self):
-        return ('articles:article_details', [], {'slug': self.slug})
+        return reverse('articles:article_details', kwargs={'slug': self.slug})
 
     def update_pageviews(self):
         """
