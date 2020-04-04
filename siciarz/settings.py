a00742b036cdf196ea176d1da7c128e996fb00f6
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-04-04 18:06:26 +0200

    Clean up MIDDLEWARE for 2.0

--- siciarz/settings.py
+++ siciarz/settings.py
@@ -34,14 +34,13 @@ INSTALLED_APPS = (
     'pgallery',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'dj_pagination.middleware.PaginationMiddleware',
